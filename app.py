9d752916844395a446b1c3ce85c176219e8d23a0
Raphaël Tournafond <raphael.tournafond@etu.univ-smb.fr>
2022-02-11 10:09:08 +0100

    feat: add base app structure

---
+++ app.py
@@ -0,0 +1,13 @@
+class App:
+
+    def __init__(self, w, h, output_device):
+        """
+        Init launchpad mock
+
+        :param w: width of the launchpad grid
+        :param h: height of the launchpad grid
+        :param output_device:
+
+        """
+        self.size = (w, h)
+        self.output_device = output_device
