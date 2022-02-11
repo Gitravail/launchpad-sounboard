bfa9118a5f50c96ddd9908b0d4b865c742fc0420
Raphaël Tournafond <raphael.tournafond@etu.univ-smb.fr>
2022-02-11 10:11:02 +0100

    doc: add output_device definition

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ class App:
 
         :param w: width of the launchpad grid
         :param h: height of the launchpad grid
-        :param output_device:
+        :param output_device: set where the sounds are going to be played
 
         """
         self.size = (w, h)
