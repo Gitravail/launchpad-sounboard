8448936e3b9d7775be28feed6d5f13cf8de7601f
Raphaël Tournafond <raphael.tournafond@etu.univ-smb.fr>
2022-02-11 10:10:22 +0100

    feat: add base window structure

---
+++ window.py
@@ -0,0 +1,13 @@
+class Window:
+
+    def __init__(self, w, h, theme):
+        """
+        Init launchpad mock
+
+        :param w: width of the launchpad grid
+        :param h: height of the launchpad grid
+        :param theme: dark or white
+
+        """
+        self.size = (w, h)
+        self.theme = theme
